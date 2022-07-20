e460c5fd288ce6b85abc05650dbfef42c5c0bf90
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-07-21 01:43:24 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+const App = () => {
+    const [image, setImage] = useState("");
+
+    useEffect(() => {
+            fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then(data => {
+                setImage(data.message)
+                })
+    }, [])
+    if (!image) {
+        return <p>Loading...</p>
+    }
+    return (<img src={ image } alt="A Dog's image"></img>
+    );
+};
+
+export default App;
